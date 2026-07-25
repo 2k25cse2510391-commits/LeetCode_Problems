bf846d111cb957c8bbdf42142388fe1f912b07c8
Mayank Srivastava <2k25cse2510391@gmail.com>
2026-07-26 00:29:11 +0530

    Time: 5 ms (53.79%), Space: 46.5 MB (77.51%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,12 +1,14 @@
 class Solution {
     public String convert(String s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
         List<char[]> arr = new ArrayList<>();
         int count =0;
         while(count != s.length()){
             count=ver(arr,numRows,count,s);
             count = dig(arr,s,numRows,count);
         }
-
         return print(arr,numRows);
     }
 
@@ -23,15 +25,15 @@ class Solution {
         return point;
     }
     public static int dig(List<char[]> arr , String str , int row, int point){
+        char[] ch = new char[row];
         for(int a = 0 ; a<row-2 ; a++){
             if(point==str.length()){
                 break;
-            }
-            char[] ch = new char[row];
+            }            
             ch[ch.length-2-a]=str.charAt(point);
             point++;
-            arr.add(ch);
         }
+        arr.add(ch);
         return point;
     }
 
